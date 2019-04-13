6ee6ca07140dbd1911a95ed70590ee2b83e2910b
Aish Raj Dahal <aish@aishraj.com>
2019-04-13 12:17:14 -0700

    Fix listing

--- app/apps/root.rb
+++ app/apps/root.rb
@@ -5,7 +5,7 @@ class RootApp < BaseApp
   end
 
   get "/training" do
-    training_id = params[:training_id]
+    training_id = params[:training_id].to_i
     @training_statuses = UserTrainingStatus.where(training_id: training_id)
     if @training_statuses
       json response: {
@@ -28,11 +28,29 @@ class RootApp < BaseApp
     end
   end
 
+  #currently sends invite to users 1-10 for the given training id
+  post '/sendinvite' do
+    user_ids = 1...10
+    @training_statuses = user_ids.map do |user_id|
+      status = UserTrainingStatus.find_or_initialize_by user_id: user_id, training_id: @body[:training_id].to_i, status: 0
+      status.save
+      status
+    end
+    json response: {
+      status: :ok,
+      errors: [],
+      data: @training_statuses,
+      request: @body,
+      redirect: nil,
+    }
+  end
+
   post '/training' do
-    @user_training = UserTrainingStatus.create_or_update user_id: @body[:user_id],
+    @user_training = UserTrainingStatus.find_or_initialize_by user_id: @body[:user_id],
                         training_id: @body[:training_id],
                         status: @body[:status]
     if @user_training.valid?
+      @user_training.save
       json response: {
         status: :ok,
         errors: [],
